7dfa904c064fa45ce4756be84e84bebcf3d01fa3
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-08-07 22:02:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello f</h1>
+        <h1>Hello fва</h1>
       </header>
     </div>
   );
