f56511a3712c02d921325a8fddce97f6050e213e
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-08-08 12:04:25 +0100

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -6,16 +6,28 @@ import { Suspense } from 'react';
 const SharedLayout = () => {
   return (
     <>
-      <nav style={{backgroundColor:'pink'}}>
-        <h2 >Header</h2>
-        <NavLink to='/' style={{marginRight:10}}>Main</NavLink>
-        <NavLink to='/registration'  style={{marginRight:10}}>Registration</NavLink>
-        <NavLink to='/login'  style={{marginRight:10}}>Login</NavLink>
+      <header>
+        <nav style={{ backgroundColor: 'pink' }}>
+          <h2>Header</h2>
+          <NavLink to="/" style={{ marginRight: 10 }}>
+            Main
+          </NavLink>
+          <NavLink to="/registration" style={{ marginRight: 10 }}>
+            Registration
+          </NavLink>
+          <NavLink to="/login" style={{ marginRight: 10 }}>
+            Login
+          </NavLink>
+        </nav>
+      </header>
+      
+      <main>
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </main>
 
-      </nav>
-      <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
+      <footer>Footer</footer>
     </>
   );
 };
